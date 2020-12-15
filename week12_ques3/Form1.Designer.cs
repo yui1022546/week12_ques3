bf9bdca7ec25e8cf987588d32341f152cfde2606
HaryKali <yui1022@163.com>
2020-12-15 21:18:13 +0800

    添加项目文件。

---
+++ week12_ques3/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace week12_ques3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.takeactionBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Location = new System.Drawing.Point(32, 36);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(75, 23);
+            this.loadBtn.TabIndex = 0;
+            this.loadBtn.Text = "装载文件";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // takeactionBtn
+            // 
+            this.takeactionBtn.Location = new System.Drawing.Point(147, 36);
+            this.takeactionBtn.Name = "takeactionBtn";
+            this.takeactionBtn.Size = new System.Drawing.Size(75, 23);
+            this.takeactionBtn.TabIndex = 1;
+            this.takeactionBtn.Text = "对文件进行修改并保存";
+            this.takeactionBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 98);
+            this.Controls.Add(this.takeactionBtn);
+            this.Controls.Add(this.loadBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadBtn;
+        private System.Windows.Forms.Button takeactionBtn;
+    }
+}
+
---
+++ week12_ques3/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace week12_ques3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+            
+            XmlDocument file = new XmlDocument();
+
+            file.Load(@"C:\Users\yui10\Desktop\week12_csharp\Bookstore.xml");
+            XmlNode book_node = file.SelectSingleNode("bookstore");
+            XmlElement xe1 = file.CreateElement("book");
+            xe1.SetAttribute("category", "children");
+
+            XmlElement xesub1 = file.CreateElement("title");
+            xesub1.InnerText = "Huluwa";//设置文本节点
+            xe1.AppendChild(xesub1);//添加到<Node>节点中
+            XmlElement xesub2 = file.CreateElement("author");
+            xesub2.InnerText = "测试作者";
+            xe1.AppendChild(xesub2);
+            XmlElement xesub4 = file.CreateElement("year");
+            xesub4.InnerText = "2001";
+            xe1.AppendChild(xesub4);
+            XmlElement xesub3 = file.CreateElement("price");
+            xesub3.InnerText = "价格";
+            xe1.AppendChild(xesub3);
+            book_node.AppendChild(xe1);
+
+
+
+            XmlNodeList someBooks = book_node.SelectNodes("/bookstore/book[price<30]");
+            for (int i = 0; i < someBooks.Count; i++)
+            {
+                someBooks.Item(i).ParentNode.RemoveChild(someBooks.Item(i));
+            }
+            Console.WriteLine(file.OuterXml);
+            file.Save("demo3.xml");
+
+
+
+        }
+    }
+}
